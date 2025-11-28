3a38a6a623fc1cec57dd18b0de865a1499d9d137
humannocturnal <syaputrar374@gmail.com>
2025-11-28 10:03:33 +0700

    tambah implementasi DatabaseSiswa untuk entity Siswa

--- app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -1,4 +1,23 @@
 package com.example.roomdatabase.room
 
-class DatabaseSiswa {
+import android.R.attr.version
+import android.content.Context
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDataBase() {
+    abstract fun siswaDao() : SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDataBase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(this){
+                Room.DatabaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    "Siswa_Database")
+                    .build().also { Instance=it }
+            })
+        }
+    }
 }
