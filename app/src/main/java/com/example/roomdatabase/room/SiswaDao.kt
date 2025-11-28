5955b01158b4f70375737ca9a35b73680dd3276e
humannocturnal <syaputrar374@gmail.com>
2025-11-28 09:46:48 +0700

    tambah SiswaDao berisi query list dan fungsi insert

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -1,4 +1,13 @@
 package com.example.roomdatabase.room
 
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
 interface SiswaDao {
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa() : Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
 }
