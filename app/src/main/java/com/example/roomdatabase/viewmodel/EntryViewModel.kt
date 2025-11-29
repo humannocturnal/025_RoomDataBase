2e5e3b55ba159bd4d30447706ae0abc7850bb61c
humannocturnal <syaputrar374@gmail.com>
2025-11-29 13:58:07 +0700

    Menambahkan EntryViewModel, UIStateSiswa, dan fungsi konversi Siswa

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,66 @@
+package com.example.roomdatabase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.repositori.RepositorySiswa
+import com.example.roomdatabase.room.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositorySiswa) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(detailSiswa: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(detailSiswa) {
+            nama.isNotBlank() && alamat.isNotBlank() && telepon.isNotBlank()
+        }
+    }
+
+    fun updateUIState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telepon: String = ""
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telepon
+)
+
+fun Siswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
+    UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telepon
+)
