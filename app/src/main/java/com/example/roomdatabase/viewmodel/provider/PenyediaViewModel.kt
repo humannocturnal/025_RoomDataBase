b469da5a2161c4880301d057d9bc6c9d402120f0
humannocturnal <syaputrar374@gmail.com>
2025-11-29 14:05:58 +0700

    Membuat komponen TopAppBar khusus Siswa

--- app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.EntryViewModel
 import com.example.roomdatabase.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
@@ -19,9 +20,5 @@ object PenyediaViewModel {
     }
 }
 
-/**
- * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
- * instance dari [AplikasiSiswa].
- */
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
