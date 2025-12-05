30755667118ac3ba9ae15166234c304fc96e92e4
humannocturnal <syaputrar374@gmail.com>
2025-12-05 14:13:28 +0700

    memperbaiki DestinasiEditSiswa dengan route dan argumen idSiswa

--- app/src/main/java/com/example/roomdatabase/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiEditSiswa.kt
@@ -1,10 +1,10 @@
 package com.example.roomdatabase.view.route
+
 import com.example.roomdatabase.R
 
-object DestinasiDetailSiswa : DestinasiNavigasi {
-    override val route = "detail_siswa"
-    override val titleRes = R.string.detail_siswa
-
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
     const val itemIdArg = "idSiswa"
     val routeWithArgs = "$route/{$itemIdArg}"
 }
